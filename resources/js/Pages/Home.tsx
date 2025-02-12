a02a0fe7d9ddad220b2c335ed1972e26dfd8f454
cecco25 <cecchinimichael.code@gmail.com>
2025-02-12 19:22:27 +0100

    Quick fix and migration

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home"></Head>
+            <div>
+                <h1>Home</h1>
+                <p>Welcome to your new Inertia app!</p>
+            </div>
+        </>
+    );
+}
